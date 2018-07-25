99a2e5103cba556e24c6fed2b4a9cbb347434526
Akhliskun <labicidanut@gmail.com>
2018-07-25 09:04:40 +0300

    Removed yosemite78 and 124 from machines with known issues

--- client.py
+++ client.py
@@ -174,16 +174,6 @@ machines_to_ignore = {
             }
         },
         "ssh_unresponsive": {
-            "t-yosemite-r7-078": {  # TODO: Make bug!
-                "bug": "",
-                "date": "15.07.2018",
-                "update": "New bug, no updates yet."
-            },
-            "t-yosemite-r7-124": {  # TODO: Make bug!
-                "bug": "",
-                "date": "15.07.2018",
-                "update": "New bug, no updates yet."
-            },
             "t-yosemite-r7-130": {  # TODO: Make bug!
                 "bug": "",
                 "date": "15.07.2018",
