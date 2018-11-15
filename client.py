6db8e4679fa81c1385e3dfd86cd394419309fb0b
Akhliskun <labicidanut@gmail.com>
2018-11-15 18:07:06 +0200

    TWC v1.0.0 - release!
    	- Now using the Heroku app created by :jhford
    	- Now using the Moonshots Invetory spreadsheet from Google Drive
    	- Everything was been re-written! New functions, New menues, New functionality.
    
    	For version 1.0.0 only "Check ALL WorkerTypes" is been implemented! Everything else will come in future releases.

--- client.py
+++ client.py
@@ -1,83 +1,39 @@
-"""
-This script will check for missing moonshots in TaskCluster.
-github repo: https://github.com/Akhliskun/taskcluster-worker-checker
-"""
-try:
-    import os
-    import json
-    import gspread
-    import prettytable
+#!/usr/bin/python3
+import sys
+import json
+import gspread
+import requests
+from prettytable import PrettyTable
+from datetime import datetime, timedelta
+from twc_modules import configuration, main_menu
+from oauth2client.service_account import ServiceAccountCredentials
+timenow = datetime.utcnow()
 
-    import urllib.request
-    from argparse import ArgumentParser
-    from collections import namedtuple
-    from oauth2client.service_account import ServiceAccountCredentials
-except ImportError:
-    print("Detected Missing Dependences! \n Please run the following, based on your OS type.")
-    print("Windows: \npip install prettytable gspread oauth2client pyopenssl")
-    print("Linux/OSX: \npip3 install prettytable gspread oauth2client pyopenssl")
-    exit(0)
+twc_version = configuration.VERSION
+lazy_time = configuration.LAZY
 
 
-workersList = []  # Used by TaskCluster API Logic
+def get_heroku_last_seen():
+    start = datetime.now()
+    url = "http://releng-hardware.herokuapp.com/machines"
+    headers = {"user-agent": "ciduty-twc/{}".format(twc_version)}
+    data = json.loads(requests.get(url, headers=headers).text)
+    heroku_machines = {}
+    for value in data:
+        idle = timenow - datetime.strptime(value["lastseen"], "%Y-%m-%dT%H:%M:%S.%f")
+        _idle = int(idle.total_seconds())
+        heroku_machines.update(
+            {value["machine"].lower(): {"lastseen": value["lastseen"], "idle": _idle,
+                                        "datacenter": value["datacenter"]}})
 
-all_osx_machines = []
-all_linux_machines = []
-all_windows_machines = []
-all_google_machines = []
-machines_to_ignore = []
-
-LINUX = "gecko-t-linux-talos"
-WINDOWS = "gecko-t-win10-64-hw"
-MACOSX = "gecko-t-osx-1010"
+    save_json("heroku_dict.json", heroku_machines)
+    end = datetime.now()
+    print("Heroku Data Processing took:", end - start)
+    return heroku_machines
 
 
-def parse_taskcluster_json(workertype):
-    '''
-    We need this incase Auth fails.
-    :param workertype: gecko-t-linux-talos, gecko-t-win10-64-hw, gecko-t-osx-1010.
-    :return: A JSON file containing all workers for a workertype selected at runtime.
-    '''
-
-    # Setup API URLs
-    if (workertype == LINUX) or (workertype == "linux"):
-        apiUrl = "https://queue.taskcluster.net/v1/provisioners/releng-hardware/worker-types/gecko-t-linux-talos/workers"
-
-    elif (workertype == WINDOWS) or (workertype == "win"):
-        apiUrl = "https://queue.taskcluster.net/v1/provisioners/releng-hardware/worker-types/gecko-t-win10-64-hw/workers"
-
-    elif (workertype == MACOSX) or (workertype == "osx"):
-        apiUrl = "https://queue.taskcluster.net/v1/provisioners/releng-hardware/worker-types/gecko-t-osx-1010/workers"
-
-    else:
-        print("ERROR: Unknown worker-type!")
-        print("Please run the script with the [client.py -h] to see the help docs!")
-        exit(0)
-
-    with urllib.request.urlopen(apiUrl, timeout=10) as api:
-        try:
-            data = json.loads(api.read().decode())
-
-        except:
-            print("TIMEOUT: JSON response didn't arive in 10 seconds!")
-            exit(0)
-
-        try:
-            if not data["workers"]:
-                # TaskCluster has an issue with RelEng hardware. BUG 1497560
-                print("Empty Worker List. Retrying...")
-                parse_taskcluster_json(workertype)
-            else:
-                for workers in data['workers']:
-                    workersList.append(workers['workerId'].lower())
-
-        except KeyboardInterrupt:
-            print("Application stopped via Keyboard Shortcut.")
-            exit(0)
-
-    return workersList
-
-def google_auth():
+def get_google_spreadsheet_data():
+    start = datetime.now()
     # Define READONLY scopes needed for the CLI
     scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly", "https://www.googleapis.com/auth/drive.readonly"]
 
@@ -87,158 +43,196 @@ def google_auth():
 
     # Authenticate / Login
     auth_token = gspread.authorize(login_info)
-    return auth_token
 
-def get_moonshot_spreadsheet_data():
-    moonshots_sheet_mdc1 = google_auth().open("Moonshot Master Inventory").get_worksheet(0)
-    moonshots_sheet_mdc2 = google_auth().open("Moonshot Master Inventory").get_worksheet(1)
+    # Choose which sheets from the SpreadSheet we will work with.
+    moonshots_sheet_mdc1 = auth_token.open("Moonshot Master Inventory").get_worksheet(0)
+    moonshots_sheet_mdc2 = auth_token.open("Moonshot Master Inventory").get_worksheet(1)
+    osx_sheet_all_mdc = auth_token.open("Moonshot Master Inventory").get_worksheet(2)
 
+    # Read the Data from the sheets
     moonshots_mdc1 = moonshots_sheet_mdc1.get_all_records()
     moonshots_mdc2 = moonshots_sheet_mdc2.get_all_records()
-
-    moonshots_google_data_mdc1 = [(entry["Hostname prefix"], entry["Hostname"], entry["Chassis"], entry["Cartridge #"],
-                                   entry["ilo ip:port"], entry["Ownership"], entry["Ownership Reason"], entry["NOTES"],
-                                   entry["CiDuty CLI Ignore"]) for entry in moonshots_mdc1]
-    moonshots_google_data_mdc2 = [(entry["Hostname prefix"], entry["Hostname"], entry["Chassis"], entry["Cartridge #"],
-                                   entry["ilo ip:port"], entry["Ownership"], entry["Ownership Reason"], entry["NOTES"],
-                                   entry["CiDuty CLI Ignore"]) for entry in moonshots_mdc2]
-
-    all_moonshots_data = moonshots_google_data_mdc1 + moonshots_google_data_mdc2
-    return all_moonshots_data
-
-def get_osx_spreadsheet_data():
-    osx_sheet_all_mdc = google_auth().open("Moonshot Master Inventory").get_worksheet(2)
     osx_all_mdc = osx_sheet_all_mdc.get_all_records()
 
-    osx_google_data = [(entry["Hostname"], entry["Serial"], entry["Warranty End Date"],entry["Ownership"],
-                        entry["Ownership Reason"], entry["Notes"], entry["CiDuty CLI Ignore"]) for entry in osx_all_mdc]
+    # Construct dictionaries with all data that we need.
+    moonshots_google_data_mdc1 = {entry["Hostname"]:
+        {
+            "prefix": entry["Hostname prefix"],
+            "chassis": entry["Chassis"],
+            "cartridge": entry["Cartridge #"],
+            "ilo": entry["ilo ip:port"],
+            "owner": entry["Ownership"],
+            "reason": entry["Ownership Reason"],
+            "notes": entry["NOTES"],
+            "ignore": entry["CiDuty CLI Ignore"]
+        } for entry in moonshots_mdc1}
 
-    return osx_google_data
+    moonshots_google_data_mdc2 = {entry["Hostname"]:
+        {
+            "prefix": entry["Hostname prefix"],
+            "chassis": entry["Chassis"],
+            "cartridge": entry["Cartridge #"],
+            "ilo": entry["ilo ip:port"],
+            "owner": entry["Ownership"],
+            "reason": entry["Ownership Reason"],
+            "notes": entry["NOTES"],
+            "ignore": entry["CiDuty CLI Ignore"]
+        } for entry in moonshots_mdc2}
 
-def name_touple_values():
-    moonshot_field_names = namedtuple("Moonshots", ["prefix", "hostname", "chassis", "cartridge", "ilo", "owner", "reason", "note", "ignore"])
-    osx_field_names = namedtuple("OSX", ["hostname", "serial", "warranty", "owner", "reason", "note", "ignore"])
+    osx_google_data = {entry["Hostname"]:
+        {
+            "serial": entry["Serial"],
+            "warranty": entry["Warranty End Date"],
+            "owner": entry["Ownership"],
+            "reason": entry["Ownership Reason"],
+            "notes": entry["Notes"],
+            "ignore": entry["CiDuty CLI Ignore"]
+        } for entry in osx_all_mdc}
 
-    # Generate List of all machines.
-    for entry in get_moonshot_spreadsheet_data():
-        row = moonshot_field_names._make(entry)
-        all_google_machines.append(row)
+    all_google_machine_data = {**moonshots_google_data_mdc1, **moonshots_google_data_mdc2, **osx_google_data}
+    save_json('google_dict.json', all_google_machine_data)
+    end = datetime.now()
+    print("Google Data Processing took:", end - start)
+    return all_google_machine_data
 
-        # Generate Moonshot list of machines to ignore.
-        if row.ignore == "Yes":
-            machines_to_ignore.append(row)
 
-    for entry in get_osx_spreadsheet_data():
-        row = osx_field_names._make(entry)
-        all_google_machines.append(row)
+def open_json(file_name):
+    with open("json_data/{}".format(file_name)) as f:
+        data = json.load(f)
+    return data
 
-        # Generate OSX list of machines to ignore.
-        if row.ignore == "Yes":
-            machines_to_ignore.append(row)
 
-    for row in all_google_machines[:600]:
-        if row.prefix == "t-w1064-ms":
-            all_windows_machines.append(row.hostname)
-        elif row.prefix == "t-linux64-ms":
-            all_linux_machines.append(row.hostname)
-        elif row.prefix == "":
-            pass
+def save_json(file_name, data):
+    with open("json_data/{}".format(file_name), 'w') as f:
+        json.dump(data, f, indent=2, sort_keys=True)
+        f.close()
+
+
+def remove_fqdn_from_machine_name():
+    start = datetime.now()
+    # Update Machine-Key from FQDN to Hostname
+    _google_dict = open_json('google_dict.json')
+    for key in list(_google_dict):
+        if len(key) > 1:
+            if "t-linux64-ms-" in key:
+                _google_dict[key[:16]] = _google_dict.pop(key)
+            elif "t-w1064-ms-" in key:
+                _google_dict[key[:14]] = _google_dict.pop(key)
+            else:
+                _google_dict[key[:17]] = _google_dict.pop(key)
+    save_json('google_dict.json', _google_dict)
+    end = datetime.now()
+    print("Removing the FQDN took:", end - start)
+
+
+def add_idle_to_google_dict():
+    start = datetime.now()
+    heroku_data = open_json("heroku_dict.json")
+    google_data = open_json("google_dict.json")
+
+    shared_keys = set(heroku_data).intersection(google_data)
+    _temp_data = {}
+    for key in shared_keys:
+        machine_idle = {"idle": heroku_data.get(key)["idle"]}
+        google_data[key].update(machine_idle)
+
+    save_json("google_dict.json", google_data)
+    end = datetime.now()
+    print("Adding IDLE times to Google Data took:", end - start)
+
+
+def output_all_problem_machines():
+    machine_data = open_json("google_dict.json")
+    table = PrettyTable()
+    table.field_names = ["Hostname", "IDLE Time ({} hours)".format(lazy_time), "ILO", "Notes"]
+
+    for machine in machine_data:
+        hostname = machine
+        ignore = machine_data.get(machine)["ignore"]
+        notes = machine_data.get(machine)["notes"]
+        if notes == "":
+            notes = "No notes available."
         else:
-            print("Something went wrong with the Google Machines Generator")
+            pass
 
-    for row in all_google_machines[-int(len(all_google_machines) - 600):]:
-        all_osx_machines.append(row.hostname)
+        try:
+            idle = timedelta(seconds=machine_data.get(machine)["idle"])
+            ilo = machine_data.get(machine)["ilo"]
+        except KeyError:
+            ilo = "-"
+        if machine:
+            if idle > timedelta(hours=6) and ignore == "No":
+                table.add_row([hostname, idle, ilo, notes])
 
-    return all_google_machines, machines_to_ignore, all_linux_machines, all_windows_machines, all_osx_machines
-
-
-def main():
-    # Get/Set Arguments
-    parser = ArgumentParser(description="Utility to check missing moonshots form TC.")
-    parser.add_argument("-w", "--worker-type",
-                        dest="worker_type",
-                        help="Available options: gecko-t-linux-talos, linux, gecko-t-win10-64-hw, win, gecko-t-osx-1010, mac",
-                        default=LINUX,
-                        required=False)
-    parser.add_argument("-u", "--ldap-username",
-                        dest="ldap_username",
-                        help="Example: -u dlabici -- Don't include @mozilla.com!!",
-                        default="root",
-                        required=False)
-
-    parser.add_argument("-v", "--verbose",
-                        dest="verbose_enabler",
-                        help="Example: -v True",
-                        default=False,
-                        required=False)
-
-    args = parser.parse_args()
-    workertype = args.worker_type
-    ldap = args.ldap_username
-    verbose = args.verbose_enabler
-
-    parse_taskcluster_json(workertype)
-
-        # Remove machines from generated list
-    if (workertype == LINUX) or (workertype == "linux"):
-        to_ignore = []
-        for machine in all_google_machines[:600]:
-            if machine.prefix == "t-linux64-ms" and machine.ignore == "Yes":
-                to_ignore.append(machine.hostname)
-
-        a = set(to_ignore)
-        workers = [x for x in all_linux_machines if x not in a]
+    print(table)
 
 
-    if (workertype == WINDOWS) or (workertype == "win"):
-        to_ignore = []
-        for machine in all_google_machines[:600]:
-            if machine.prefix == "t-w1064-ms" and machine.ignore == "Yes":
-                to_ignore.append(machine.hostname)
-
-        a = set(to_ignore)
-        workers = [x for x in all_windows_machines if x not in a]
+def write_html_data():
+    pass
 
 
-    if (workertype == MACOSX) or (workertype == "osx"):
-        to_ignore = []
-        for machine in all_google_machines[-int(len(all_google_machines) - 600):]:  # Pick only OSX machines (last 450 entries)
-            if machine.ignore == "Yes":
-                to_ignore.append(machine.hostname)
-
-        a = set(to_ignore)
-        workers = [x for x in all_osx_machines if x not in a]
+def push_html_to_git():
+    pass
 
 
-    b = set(workersList)
-    worker_list = []
-
-    if (workertype == WINDOWS) or (workertype == "win"):
-        for machine in workers:
-            worker_list.append(machine[:14])
-        missing_machines = [x for x in worker_list if x not in b]
-        print("Total Missing Machines: ",len(missing_machines))
-        for machine in missing_machines:
-            print(machine)
-
-    if (workertype == LINUX) or (workertype == "linux"):
-        for machine in workers:
-            worker_list.append(machine[:16])
-        missing_machines = [x for x in worker_list if x not in b]
-        print("Total Missing Machines: ", len(missing_machines))
-        for machine in missing_machines:
-            print(machine)
-
-    if (workertype == MACOSX) or (workertype == "osx"):
-        for machine in workers:
-            worker_list.append(machine[:17])
-        missing_machines = [x for x in worker_list if x not in b]
-        print("Total Missing Machines: ", len(missing_machines))
-        for machine in missing_machines:
-            print(machine)
+def run_all_machines():
+    """
+    This is the main order in which the tool will run all the functions needed to return the result.
+      - If new features are added, which needs to run WITHOUT a parameter, it needs to be added here.
+    """
+    get_heroku_last_seen()
+    get_google_spreadsheet_data()
+    remove_fqdn_from_machine_name()
+    add_idle_to_google_dict()
+    output_all_problem_machines()
 
 
+def run_windows_machines():
+    print("Logic not implemented yet!")
+    exit(0)
 
-if __name__ == '__main__':
-    name_touple_values()
-    main()
+
+def run_linux_machines():
+    print("Logic not implemented yet!")
+    exit(0)
+
+
+def run_yosemite_machines():
+    print("Logic not implemented yet!")
+    exit(0)
+
+
+def dev_run_login():
+    """
+    When debuging and you change how data is stored/manipulated, use this function to always recreate the files.
+    This will also skip the MainMenu of the CLI application
+    :return: Fresh data.
+    """
+    get_heroku_last_seen()
+    get_google_spreadsheet_data()
+    remove_fqdn_from_machine_name()
+    add_idle_to_google_dict()
+    output_all_problem_machines()
+
+
+if __name__ == "__main__":
+    script_start = datetime.now()
+
+    if ("-v" or "--verbose") in sys.argv:
+        verbose = True
+        print("Script running with Verbose Mode ENABLED\n")
+    else:
+        verbose = False
+
+    if ("-ct" or "--citest") in sys.argv:
+        citest = True
+        print("TravisCI Testing Begins!")
+        dev_run_login()
+    else:
+        citest = False
+        main_menu.run_menu()
+
+    script_end = datetime.now()
+
+    if verbose:
+        print("Script total runtime:", script_end - script_start)
---
+++ twc_modules/main_menu.py
@@ -0,0 +1,87 @@
+from twc_modules.configuration import VERSION
+from client import run_all_machines, run_windows_machines, run_linux_machines, run_yosemite_machines
+
+
+def run_menu(*arg):
+    """
+    - Check Machine(s) Status
+    |-- Check ALL WorkerTypes.
+    |-- Check Only Windows machines
+    |-- Check Only Linux machines
+    |-- Check Only Yosemite machines
+
+    - Check a Specific Machine
+
+    - Check machines Loaned by user
+    -- input(ldap)
+
+    - List all Machines with added Notes.
+    """
+    print("Welcome to CiDuty's TaskCluster Worker Checker.\n"
+          "You can use the options below to investigate the machines which you want.\n"
+          "TWC version: {} || Github: https://github.com/Akhliskun/taskcluster-worker-checker\n".format(VERSION))
+    print("1. Check Machine(s) Status\n"
+          "2. Check a Specific Machine\n"
+          "3. List Machines Loaned by User\n"
+          "4. List Machines with Added Notes\n"
+          "5. TaskCluster Worker Checker HELP Docs\n\n"
+          "0. Exit application.")
+    try:
+        choice = int(input())
+    except ValueError:
+        print("\n\nInvalid Choice!\n"
+              "Restarting script!\n\n")
+        run_menu()
+
+    if choice == 1:
+        print("1. Check ALL WorkerTypes\n"
+              "2. Check ONLY Windows Machines\n"
+              "3. Check ONLY Linux Machines\n"
+              "4. Check ONLY Yosemite Machines\n\n"
+              "0. Go Back.")
+        try:
+            choice_menu1 = int(input())
+        except ValueError:
+            print("\n\nInvalid Choice!\n")
+            run_menu()
+        if choice == 1:
+            run_all_machines()
+            exit(0)
+        if choice_menu1 == 2:
+            run_windows_machines()
+        if choice_menu1 == 3:
+            run_linux_machines()
+        if choice_menu1 == 4:
+            run_yosemite_machines()
+        if choice_menu1 == 0:
+            run_menu()
+
+    if choice == 11:
+        run_all_machines()
+    if choice in range(11, 16):
+        print("Logic not implemented yet!")
+        exit(0)
+
+    if choice == 2:
+        print("Logic not implemented yet!")
+        exit(0)
+
+    if choice == 3:
+        print("Logic not implemented yet!")
+        exit(0)
+
+    if choice == 4:
+        print("Logic not implemented yet!")
+        exit(0)
+
+    if choice == 5:
+        print("Logic not implemented yet!")
+        exit(0)
+
+    if choice == 0:
+        print("Closing CLI application.\n")
+        exit()
+    else:
+        print("\n\nInvalid Choice!\n"
+              "Restarting script!\n\n")
+        run_menu()
