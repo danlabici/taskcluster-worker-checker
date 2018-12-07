7b6d11c124992d54db8b8968932b19d6db8454a2
Bolchis Bogdan <bogdan.bolchis@softvision.ro>
2018-12-07 14:16:48 +0200

    filter data fixed

--- ui/details.py
+++ ui/details.py
@@ -3,7 +3,7 @@ from PyQt5.QtCore import pyqtSlot
 import os
 from datetime import timedelta
 from ui.messaging_module import TrayIcon
-from ui.modules import open_json
+from ui.modules import open_json, VmMachine
 
 class MachineDetails(QtWidgets.QDialog):
     def __init__(self):
@@ -12,29 +12,41 @@ class MachineDetails(QtWidgets.QDialog):
         uic.loadUi(file_path, self)
         TrayIcon().messageInfo("Machine Details", "Load Complete.", 1)
         self.close_btn.pressed.connect(self.close)
+        self.objects = []
+        self.add_lista()
+
+    def add_lista(self):
+        self.objects.clear()
+        machine_data = open_json("google_dict.json")
+        idle_data = open_json("heroku_dict.json")
+        for machine, _idle in zip(machine_data, idle_data):
+            try:
+                ilo = machine_data.get(machine)["ilo"]
+            except KeyError:
+                ilo = "N/A"
+            t = VmMachine(machine)
+            t.insert_data(machine_data.get(machine)["ignore"],
+                          machine_data.get(machine)["notes"],
+                          machine_data.get(machine)["serial"],
+                          machine_data.get(machine)["owner"],
+                          machine_data.get(machine)["reason"],
+                          idle_data.get(_idle)["idle"],
+                          ilo)
+            self.objects.append(t)
 
     @pyqtSlot(str)
     def display_info(self, filter):
-        machine_data = open_json("google_dict.json")
-        idle_data = open_json("heroku_dict.json")
-        for machine, idle in zip(machine_data, idle_data):
-            if filter in machine:
-                hostname = machine
-                ignore = machine_data.get(machine)["ignore"]
-                notes = machine_data.get(machine)["notes"]
-                serial = machine_data.get(machine)["serial"]
-                owner = machine_data.get(machine)["owner"]
-                reason = machine_data.get(machine)["reason"]
-                idle = str(timedelta(seconds=idle_data.get(idle)["idle"]))
+        for member in self.objects:
+            if filter in member.hostname:
                 try:
-                    ilo = machine_data.get(machine)["ilo"]
+                    ilo = member.ilo
                 except KeyError:
                     ilo = "-"
-                self.key_line.setText(hostname)
-                self.idle_line.setText(idle)
+                self.key_line.setText(member.hostname)
+                self.idle_line.setText(str(timedelta(seconds=member.idle)))
                 self.ilo_line.setText(ilo)
-                self.sn_line.setText(serial)
-                self.owner_line.setText(owner)
-                self.reason_line.setText(reason)
-                self.notes_line.setText(notes)
-                self.ignore_line.setText(ignore)
+                self.sn_line.setText(member.serial)
+                self.owner_line.setText(member.owner)
+                self.reason_line.setText(member.reason)
+                self.notes_line.setText(member.notes)
+                self.ignore_line.setText(member.ignore)
